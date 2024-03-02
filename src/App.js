8443084e584eee1a2cce7b77b70d06b931b1e654
Rohan Kumar Dwivedi <rohandwivedi1203@gmail.com>
2024-03-02 17:21:32 +0530

    Expense

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useRef, useEffect } from 'react'
+import { Grid } from "@material-ui/core";
+import { PushToTalkButton, PushToTalkButtonContainer, ErrorPanel } from '@speechly/react-ui';
+import Details from './components/Details/Details'
+import Main from './components/Main/Main'
+import useStyles from './styles';
+
+import { SpeechState, useSpeechContext } from "@speechly/react-client";
+
+const App = () => {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <Grid className={classes.grid} container spacing={0} alignItems="center" style={{ height: '100vh' }}>
+                <Grid item xs={12} sm={3} className={classes.mobile}>
+                    <Details title="Income" />
+                </Grid>
+                {
+
+                }
+                <Grid item xs={12} sm={5} className={classes.main}>
+                    <Main />
+                </Grid>
+                <Grid item xs={12} sm={3} className={classes.desktop}>
+                    <Details title="Income" />
+                </Grid>
+                <Grid item xs={12} sm={3} className={classes.last}>
+                    <Details title="Expense" />
+                </Grid>
+            </Grid>
+            <PushToTalkButtonContainer>
+                <PushToTalkButton />
+                <ErrorPanel />
+            </PushToTalkButtonContainer>
+        </div>
+    )
+}
+
+export default App
---
+++ src/context/context.js
@@ -0,0 +1,34 @@
+import React, { useReducer, createContext } from 'react';
+import contextReducer from './contextReducer';
+
+const initialState = JSON.parse(localStorage.getItem('transactions')) || [{ "amount": 100, "category": "Salary", "type": "Income", "date": "2021-05-24", "id": "e9309fff-3a78-4648-8cf6-d1dbcf509f8b" }];
+
+export const ExpenseTrackerContext = createContext(initialState);
+
+export const Provider = ({ children }) => {
+    const [transactions, dispatch] = useReducer(contextReducer, initialState)
+
+    console.log(transactions);
+    //Action creators
+    const deleteTransaction = (id) => {
+        dispatch({ type: 'DELETE_TRANSACTION', payload: id });
+    }
+
+    const addTransaction = (transaction) => {
+        dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
+    }
+
+    const balance = transactions.reduce((acc, curVal) =>
+        curVal.type === 'Expense' ? acc - curVal.amount : acc + curVal.amount, 0);
+
+    return (
+        <ExpenseTrackerContext.Provider value={{
+            deleteTransaction,
+            addTransaction,
+            transactions,
+            balance
+        }}>
+            {children}
+        </ExpenseTrackerContext.Provider>
+    )
+}
